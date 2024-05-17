ba03d458b203bea8c75a671edfe4940e9ef4e01f
velho4 <vilhoseppanen8@gmail.com>
2024-05-17 13:45:39 +0300

    Add files via upload

--- treidausalgo.py
+++ treidausalgo.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import date, timedelta
 import numpy as np
+import time
 import pandas as pd
 import pandas_ta as ta
 import alpaca_trade_api as tradeapi
@@ -14,10 +15,10 @@ API_KEY = config.API_KEY
 SEC_KEY = config.SEC_KEY
 
 class API_liikenne:
-    def __init__(self, api_key, sec_key):
+    def __init__(self, api_key, sec_key, osake):
         self.api_key = api_key
         self.sec_key = sec_key
-        self.osake = config.osakkeet 
+        self.osake = osake 
         self.asiakas = TradingClient(self.api_key, self.sec_key, paper=True)
         self.headers = headers = {'Accept': 'application/json','APCA-API-KEY-ID' : self.api_key,'APCA-API-SECRET-KEY' : self.sec_key,'Content-Type': 'application/json'}
     
@@ -44,18 +45,24 @@ class API_liikenne:
                 paivat.append(data['t'])
         
         return pd.DataFrame({'Symbol': symbolit, 'Day': paivat, 'Close':sulkuhinnat})
+    
+    def viimeisin_hinta(self):
+        payload = {}
+        url = 'https://data.alpaca.markets/v2/stocks/' + self.osake + '/trades/latest'
+        response = requests.request("GET", url, headers=self.headers, data=payload)
+        response = response.json()
+        return response["trade"]["p"]
 
 
-    def Toimeksianto(self, market_order_data):
+    def toimeksianto(self, market_order_data):
         
         self.asiakas.submit_order(order_data=market_order_data)
 
     def tilin_data(self):
         payload = {}
-        response = requests.request("GET", "https://paper-api.alpaca.markets/v2/account", headers=self.headers, data=payload )   
-        response = response.json()
-        print(response)
-    
+        palautus = requests.request("GET", "https://paper-api.alpaca.markets/v2/account", headers=self.headers, data=payload)   
+        palautus = palautus.json()
+        return palautus
 
     def kaikki_positiot(self):
         payload = {}
@@ -70,104 +77,106 @@ class API_liikenne:
 
 
 class Algoritmi:
-    def __init__(self):
+    def __init__(self, osake):
         
         self.api_key = API_KEY
         self.sec_key = SEC_KEY
-        self.api = API_liikenne(API_KEY, SEC_KEY)
+        self.api = API_liikenne(API_KEY, SEC_KEY, osake)
         self.osta = False
         self.myy = False
         self.kateinen = 0
-        self.hinta = 0
+        self.osakkeen_hinta = 0
+        self.rsi = 0
         self.liukuva_200 = 0
         self.liukuva_50 = 0
         self.liukuva_20 = 0
         self.liukuva_200_nousee = False
         self.liukuva_50_nousee = False
         self.liukuva_20_nousee = False
-        
-        self.hinnat = []
-        self.df = pd.DataFrame
+        self.salkun_arvo = 0
+        self.position_koon_raja = 0
+        self.df = pd.DataFrame()
     
     def data_analyysi(self):
         self.df = self.api.data_palvelimelta()
         self.df["SMA 20"] = self.df.ta.sma(20)
         self.df["SMA 50"] = self.df.ta.sma(50)
         self.df["SMA 200"] = self.df.ta.sma(200)
-        print(self.df)
-        for hinta in self.df["Close"]:
-            self.hinnat.append(hinta)
-            
-        print(self.hinnat)    
-        self.hinta = self.hinnat[-1]
-        self.laske_liukuva(200)
-        self.laske_liukuva(50)
-        self.laske_liukuva(20)
-        print(self.liukuva_200, self.liukuva_50, self.liukuva_20)
+        print(self.df.iloc[-1]["SMA 200"] > self.df.iloc[-2]["SMA 200"])
+        self.osakkeen_hinta = self.api.viimeisin_hinta()
+        self.rsi = self.df.rsi()        
     
     def tilin_tiedot(self):
         tiedot = self.api.tilin_data()
-        self.kateinen = tiedot['non_marginable_buying_power']
+        self.kateinen = float(tiedot['non_marginable_buying_power'])
+        self.salkun_arvo = tiedot["portfolio_value"]
+        self.position_koon_raja = float(self.salkun_arvo) * 0.2
+        if self.kateinen < self.position_koon_raja:
+            self.position_koon_raja = self.kateinen
         
         
-
-
-    def laske_liukuva(self, aika):
-        pituus = len(self.hinnat)
-        summa = 0
-        maara = 0
-        for hinta in self.hinnat[pituus - (aika) : pituus]:
-            summa += hinta
-            maara += 1
-        
-        if aika == 200:
-            self.liukuva_200 = summa/maara
-            return
-        elif aika == 50:
-            self.liukuva_50 = summa/maara
-            return
-        self.liukuva_20 = summa/maara
-        return
-    
-    def palauta_liukuva(self, aika):
-        pituus = len(self.hinnat)
-        summa = 0
-        maara = 0
-        for hinta in self.hinnat[pituus - (aika + 1) : pituus - 1]:
-            summa += hinta
-            maara += 1
-        
-        return summa/maara
-    
     def onko_liukuvat_nousevia(self):
-        self.liukuva_200_nousee = self.palauta_liukuva(200) < self.liukuva_200
-        self.liukuva_50_nousee = self.palauta_liukuva(50) < self.liukuva_50
-        self.liukuva_20_nousee = self.palauta_liukuva(20) < self.liukuva_20
-        print(self.liukuva_20_nousee, self.liukuva_50_nousee, self.liukuva_200_nousee)
-        print(self.palauta_liukuva(50), self.liukuva_50)
+        self.liukuva_200_nousee = self.df.iloc[-1]["SMA 200"] > self.df.iloc[-2]["SMA 200"]
+        self.liukuva_50_nousee = self.df.iloc[-1]["SMA 50"] > self.df.iloc[-2]["SMA 50"]
+        self.liukuva_20_nousee = self.df.iloc[-1]["SMA 20"] > self.df.iloc[-2]["SMA 20"]
+   
 
         
     def ostetaanko(self):
         if self.liukuva_200_nousee and self.liukuva_50_nousee and self.liukuva_20_nousee:
-            if self.hinta > self.liukuva_50 and self.hinta > self.liukuva_200:
-                if self.hinta <= self.liukuva_20:
+            if self.osakkeen_hinta > self.liukuva_50 and self.osakkeen_hinta > self.liukuva_200:
+                if self.osakkeen_hinta <= self.liukuva_20 * 1.01:
                     self.osta = True
-        
     
-    def osta(self):
+    def myydäänkö(self):
+        if se
+
+    
+    def shortataaanko(self):
+
+
+    
+    def osto(self):
+        if self.kateinen == 0:
+            return
         positiot = self.api.kaikki_positiot()
-        for positio in positiot["symbol"]:
-            if positio == self.api.osake:
-                pass
+        for indeksi, positio in positiot.iterrows():
+            if positio["symbol"] == self.api.osake:                
+                if float(positio["market_value"]) < self.position_koon_raja - self.osakkeen_hinta:
+                    osakkeiden_maara = (self.position_koon_raja - float(positio["market_value"])) // self.osakkeen_hinta
+                    order_data = MarketOrderRequest(symbol=self.api.osake, qty=osakkeiden_maara, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
+                    self.api.toimeksianto(order_data)
+                    return
+                else:
+                    return
+            
+        osakkeiden_maara = self.position_koon_raja // float(self.osakkeen_hinta)
+        print(self.position_koon_raja)
+        print(self.osakkeen_hinta)
+        print(osakkeiden_maara)
+        order_data = MarketOrderRequest(symbol=self.api.osake, qty=osakkeiden_maara, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
+        self.api.toimeksianto(order_data)        
+                
 
         
     
     
-    def silmukka(self):
-        algo = Algoritmi()
-        algo.data_analyysi()
-        algo.onko_liukuvat_nousevia()
-        algo.ostetaanko()
+def silmukka():
+    while True:
+        for osake in config.osakkeet:
+            algo = Algoritmi(osake)
+            algo.data_analyysi()
+            algo.tilin_tiedot()
+            algo.onko_liukuvat_nousevia()
+            algo.ostetaanko()
+            algo.osto()
+            
+        
+        time.sleep(10)
+
+
+
+silmukka()
 
 
 
@@ -176,22 +185,3 @@ class Algoritmi:
 
 
 
-
-
-
-
-algo = Algoritmi()
-algo.data_analyysi()
-algo.onko_liukuvat_nousevia()
-algo.ostetaanko()
-market_order_data = MarketOrderRequest(
-                    symbol="SPY",
-                    qty=1,
-                    side=OrderSide.SELL,
-                    time_in_force=TimeInForce.DAY
-                    )
-
-# Market order
-algo.api.asiakas.submit_order(order_data=market_order_data)
-algo.api.kaikki_positiot()
-algo.api.tilin_data()
